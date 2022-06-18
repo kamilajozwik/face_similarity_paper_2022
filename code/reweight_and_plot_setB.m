7fe6b5ba580e901ce47d0fe5ea4d352b3cae9425
Kate Storrs <katherine.storrs@gmail.com>
2022-06-18 18:47:51 +0200

    corrected boot and cv numbers

--- code/reweight_and_plot_setB.m
+++ code/reweight_and_plot_setB.m
@@ -11,8 +11,8 @@
 
 clear all
 
-nCV = 3; % can make smaller for debugging. At least 20 for full run.
-nboots = 5; % can make smaller for debugging. At least 1000 for full run.
+nCV = 20; % can make smaller for debugging. At least 20 for full run.
+nboots = 1000; % can make smaller for debugging. At least 1000 for full run.
 savedir = './analysis/';
 
 %% load human data
