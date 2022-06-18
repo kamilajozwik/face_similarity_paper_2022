b8bd8fdd83b0324a187f3097c32c8cab744ac5c0
Kate Storrs <katherine.storrs@gmail.com>
2022-06-18 19:36:23 +0200

    added intermodel correlation plotting script

---
+++ code/intermodel_correlations_plot.m
@@ -0,0 +1,46 @@
+% Load model data and plot correlations between predictions of each model.
+
+clear all
+
+%% load model data
+% cycle through model directory, loading predicted dissimilarities from 
+% each component and adding them to an all-models cell array
+
+modeldir = 'data/model_predictions_setA/'
+modellist = dir([modeldir,'*.csv']);
+
+allmodels = [];
+allnames = {};
+
+for thismodel = 1:length(modellist)
+    
+    % load model-predicted distances
+    rawModel = csvread(strcat(modeldir,modellist(thismodel).name));
+    if iscolumn(rawModel)
+        rawModel = rawModel';
+    end
+    
+    allmodels = [allmodels; rawModel];
+    
+    tmp = modellist(thismodel).name(1:end-4);
+    tmp(tmp=='_') = ' ';
+    allnames{thismodel} = tmp;
+end
+
+%% 
+
+RSM = squareform(pdist(allmodels,'correlation'));
+imagesc(RSM)
+axis square
+xticks(1:length(allnames));
+xticklabels(allnames);
+xtickangle(45);
+yticks(1:length(allnames));
+yticklabels(allnames);
+set(gca,'FontSize',12);
+set(gcf,'color','w');
+title('Correlation distance')
+
+set(gcf,'renderer','Painters')
+% optionally, save figure in vector format
+% print('-dpdf','myVectorFile3.pdf','-r300','-painters')
